15d57388849864986d4d0238d09784112e53f829
Nilton <niltonklebert61@gmail.com>
2023-08-28 11:55:57 -0300

    Roteização da moto

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from PySimpleGUI import PySimpleGUI as sg
 
 sg.theme('DarkGrey14')
 layout = [
-    [sg.Text('Pacotes Grandes:  '), sg.Multiline(size=(40,5), key='coord_g')],
-    [sg.Text('Pacotes Pequenos:'), sg.Multiline(size=(40,5), key='coord_p')],
-    [sg.Text('Capacidade Moto:  '), sg.InputText(size=3, key='nome')],
+    [sg.Text('Pacotes Grandes:  '), sg.Multiline(size=(42,5), key='coord_g')],
+    [sg.Text('Pacotes Pequenos:'), sg.Multiline(size=(42,5), key='coord_p')],
+    [sg.Text('Capacidade Moto:  '), sg.InputText(size=3, key='nome', default_text=0), sg.Text('QTD de Carros:'),
+     sg.InputText(size=3, key='qtd_veic', default_text=3), sg.Text('Tempo Máx(Hrs):'),
+     sg.InputText(size=3, key='tempo', default_text=3.5)],
     [sg.Text('                                        '), sg.Button('Gerar Rotas', key='Gerar_Rotas')]
 ]
 
 # Janela
-janela = sg.Window('Roteirizador', layout, icon='os.path.abspath("./Templates/icon.ico")')
+janela = sg.Window('Rota Econômica', layout, icon='icon.ico')
 
 # Ler os eventos
 while True:
